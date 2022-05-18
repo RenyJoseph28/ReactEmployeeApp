c0edf5030870254ac40ac11504b83339431f7e3c
RenyJoseph28 <renyjoseph28@gmail.com>
2022-05-18 21:40:46 +0530

    employee

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Employee from './component/Employee';
 
 
 function App() {
   return (
     <div>
-
+<Employee/>
     </div>
   );
 }
---
+++ src/component/Employee.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Employee = () => {
+  return (
+    <div>
+      <div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-2">
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="">Employee id</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="">Employee Name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="">Designation</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="">salary</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="">company name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="">Dob</label>
+                    <input type="date" name="" id="" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="">Email</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button class="btn btn-success">clear</button>
+                </div>
+                <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button class="btn btn-success">Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>  
+
+
+
+
+    </div>
+  )
+}
+
+export default Employee
